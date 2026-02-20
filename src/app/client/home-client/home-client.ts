669cbe8fa501ab92374c139ca0d57fe253818143
YulianaChambi <chambiy447@gmail.com>
2026-02-20 11:50:29 -0500

    Fix Design Home 🦖  (#29)

--- src/app/client/home-client/home-client.ts
+++ src/app/client/home-client/home-client.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-client',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home-client.html'
+  imports: [CommonModule, RouterLink],
+  templateUrl: './home-client.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeClient {
 
