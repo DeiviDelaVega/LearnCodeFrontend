acae3f991c8ba71812be3e16077cb70a60eaf400
Deivi De la Vega <deividelavegab@gmail.com>
2026-02-23 23:36:04 -0500

    Authorized Token Plans 🚀

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -3,13 +3,16 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('google_token');
 
-  const publicEndpoints = ['/api/courses', '/api/stripe/webhook'];
+  // endpoints públicos
+  const publicEndpoints = ['/api/courses', '/api/plans', '/api/stripe/webhook'];
 
   const isPublic = publicEndpoints.some((endpoint) => req.url.includes(endpoint));
 
   if (token && !isPublic) {
     req = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` },
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
     });
   }
 
